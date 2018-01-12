1ae7ca999bfe645151f16f7e25676d41d209a0e9
Michel B <michelb@JB-MBA.local>
2018-01-12 15:45:38 -0500

    Tweets returning

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.G57p1NfOr8QdDSt08QiYlYDfW,
-  consumer_secret: process.env.ogcQvYaegEIcl73j7orfYFVd8Z9sSoV89FU4BQDwjrFSU2VqDX,
-  access_token_key: process.env.DlZQYyEjznchlxVsa5ZiNKjMNSPw3Av,
-  access_token_secret: process.env.vgShrG8e86RYeUNcZobBIXL7tsQB0WOioXqfiRV069cOa
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.d60bcb24b32041f28f23e3b1ce1f2ec4,
-  secret: process.env.e5c2be7f43374250b0ddcf1bee413aae
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,23 +1,36 @@
+require("dotenv").config();
 var request = require("request");
 var inquirer = require("inquirer");
-var twitter = require("twitter");
+var Twitter = require("twitter");
 var spotify = require('node-spotify-api');
-require("dotenv").config();
 var keys = require("./keys.js");
+var userPick = process.argv[2];
 
+if (userPick === "my-tweets") {
+    console.log("Show me the tweets");
+}
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "Hello, I'm Liri. How can I help?",
-            choices: ["View Tweets", "Spotify Search", "Movie Search", "Surprise Me"],
-            name: "menu"
+    .prompt([{
+        type: "list",
+        message: "Hello, I'm Liri. How can I help?",
+        choices: ["View Tweets", "Spotify Search", "Movie Search", "Surprise Me"],
+        name: "choice"
+    }])
+    .then(function (user) {
+        if (user.choice === "View Tweets") {
+            client.get("statuses/user_timeline", {
+                    screen_name: "VirtualVisionz"
+                },
+                function (error, tweets, response) {
+                    if (!error) {
+                        tweets.forEach(function (element) {
+                            console.log("Created On: " + element.created_at + "    " + element.text);
+                        })
+                    }
+                });
         }
-    ])
-    .then(function(inquirerResponse){
-        console.log("I'm ready");
     });
